1b56bfcc4ea5b4599feb673e08b5c6d431c322c4
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2024-04-27 23:54:56 -0300

    Initial Commit

---
+++ QuestionMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuestionMe
+//
+//  Created by Paolo Prodossimo Lopes on 27/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuestionMe/QuestionMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionMeApp.swift
+//  QuestionMe
+//
+//  Created by Paolo Prodossimo Lopes on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct QuestionMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
